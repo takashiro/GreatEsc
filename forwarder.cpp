8f8776e382c6f6838051594a03bc13ce6a25bc6c
Takashiro <takashiro@qq.com>
2016-01-15 19:14:51 +0800

    Prepare to extend more functions

--- forwarder.cpp
+++ forwarder.cpp
@@ -5,11 +5,23 @@
 #include <QTcpSocket>
 #include <QUrl>
 
+QMap<QByteArray, Forwarder::CommandHandler> Forwarder::m_handlers;
+struct ForwarderInit{
+    ForwarderInit(){
+        Forwarder::m_handlers["GET"] = &Forwarder::directRequest;
+        Forwarder::m_handlers["POST"] = &Forwarder::directRequest;
+        Forwarder::m_handlers["CONNECT"] = &Forwarder::setupTunnel;
+        Forwarder::m_handlers["CHYOUSA"] = &Forwarder::setFilter;
+    }
+};
+ForwarderInit init;
+
 Forwarder::Forwarder(QTcpSocket *client, QObject *parent)
     : QObject(parent)
     , m_client(client)
     , m_proxy(nullptr)
     , m_tunnel(nullptr)
+    , m_enableFilter(false)
 {
     connect(client, &QTcpSocket::readyRead, this, &Forwarder::handleRequest);
     connect(client, &QTcpSocket::disconnected, this, &Forwarder::deleteLater);
@@ -32,100 +44,30 @@ void Forwarder::setProxy(QTcpSocket *proxy)
 
 void Forwarder::handleRequest()
 {
-    bool isChyousa = false;
     while (!m_client->atEnd()) {
-        QByteArray line = m_client->readLine();
-        if (isChyousa)
-            filterRequest(line, false);
+        QByteArray line = readLine();
 
         QTextStream stream(&line, QIODevice::ReadOnly);
         QByteArray method, rawUrl, protocol;
         stream >> method >> rawUrl >> protocol;
 
-        //HTTP GET & POST
-        if (method == "GET" || method == "POST") {
-            //Resolve host
-            QUrl url = QUrl::fromEncoded(rawUrl);
-            QString host = url.host();
-            ushort port = url.port(80);
-
-            QTcpSocket *socket = new QTcpSocket(this);
-            connect(socket, &QTcpSocket::readyRead, this, &Forwarder::forwardResponse);
-            connect(socket, &QTcpSocket::connected, [=](){
-                socket->write(method);
-                socket->write(" ");
-
-                QString relativeUrl = url.path();
-                if (url.hasQuery()) {
-                    relativeUrl.append('?');
-                    relativeUrl.append(url.query());
-                }
-                socket->write(relativeUrl.toLatin1());
-                socket->write(" ");
-                socket->write(protocol);
-                socket->write("\r\n");
-
-                QByteArray body = readBody();
-                if (isChyousa)
-                    filterRequest(body, false);
-                socket->write(body);
-            });
-            connect(socket, (void (QAbstractSocket::*)(QAbstractSocket::SocketError)) &QTcpSocket::error, [=](QAbstractSocket::SocketError){
-                m_client->disconnectFromHost();
-            });
-            socket->connectToHost(host, port);
-
-        //TCP Tunnel
-        } else if (method == "CONNECT") {
-            disconnect(m_client, &QTcpSocket::readyRead, this, &Forwarder::handleRequest);
-
-            m_tunnel = new QTcpSocket(this);
-            connect(m_tunnel, &QTcpSocket::readyRead, this, &Forwarder::forwardResponse);
-            connect(m_tunnel, (void (QAbstractSocket::*)(QAbstractSocket::SocketError)) &QTcpSocket::error, [=](QAbstractSocket::SocketError){
-                m_client->disconnectFromHost();
-            });
-
-            QList<QByteArray> parts = rawUrl.split(':');
-            QString host = QString::fromLatin1(parts.at(0));
-            ushort port = 80;
-            if (parts.length() > 1)
-                port = parts.at(1).toUShort();
-
-            QByteArray body = readBody();
-            if (isChyousa)
-                filterRequest(body, false);
-            filterRequest(body, true);
-
-            if (isBlocked(host)) {
-                filterRequest(rawUrl, true);
-
-                connect(m_tunnel, &QTcpSocket::connected, [=](){
-                    m_tunnel->write("CHYOUSA / GP\r\n");
-                    m_tunnel->write("CONNECT ");
-                    m_tunnel->write(rawUrl);
-                    m_tunnel->write(" ");
-                    m_tunnel->write(protocol);
-                    m_tunnel->write("\r\n");
-                    m_tunnel->write(body);
-
-                    connect(m_client, &QTcpSocket::readyRead, this, &Forwarder::forwardRequest);
-                });
-                m_tunnel->connectToHost("p.takashiro.me", 5526);
-            } else {
-                connect(m_tunnel, &QTcpSocket::connected, [=](){
-                    connect(m_client, &QTcpSocket::readyRead, this, &Forwarder::forwardRequest);
-                    m_client->write("HTTP/1.1 200 Connection Established\r\n\r\n");
-                });
-                m_tunnel->connectToHost(host, port);
-            }
-            return;
-        } else if (method == "CHYOUSA") {
-            isChyousa = true;
-        }
+        CommandHandler handler = m_handlers.value(method);
+        if (handler)
+            (this->*handler)(method, rawUrl, protocol);
+        else
+            qWarning("Unknown method: %s", method.constData());
     }
 }
 
-QByteArray Forwarder::readBody()
+QByteArray Forwarder::readLine()
+{
+    QByteArray line = m_client->readLine();
+    if (m_enableFilter)
+        filterRequest(line, false);
+    return line;
+}
+
+QByteArray Forwarder::readAll()
 {
     //Read the whole request
     QByteArray body;
@@ -135,6 +77,10 @@ QByteArray Forwarder::readBody()
         if (line == "\r\n" || line.isEmpty())
             break;
     }
+
+    if (m_enableFilter)
+        filterRequest(body, false);
+
     return body;
 }
 
@@ -202,4 +148,86 @@ void Forwarder::forwardResponse()
         QByteArray data = proxy->readAll();
         m_client->write(data);
     }
+}
+
+void Forwarder::directRequest(const QByteArray &method, const QByteArray &rawUrl, const QByteArray &protocol)
+{
+    QByteArray body = readAll();
+
+    //Resolve host
+    QUrl url = QUrl::fromEncoded(rawUrl);
+    QString host = url.host();
+    ushort port = url.port(80);
+
+    QTcpSocket *socket = new QTcpSocket(this);
+    connect(socket, &QTcpSocket::readyRead, this, &Forwarder::forwardResponse);
+    connect(socket, &QTcpSocket::connected, [=](){
+        socket->write(method);
+        socket->write(" ");
+
+        QString relativeUrl = url.path();
+        if (url.hasQuery()) {
+            relativeUrl.append('?');
+            relativeUrl.append(url.query());
+        }
+        socket->write(relativeUrl.toLatin1());
+        socket->write(" ");
+        socket->write(protocol);
+        socket->write("\r\n");
+
+        socket->write(body);
+    });
+    connect(socket, (void (QAbstractSocket::*)(QAbstractSocket::SocketError)) &QTcpSocket::error, [=](QAbstractSocket::SocketError){
+        m_client->disconnectFromHost();
+    });
+    socket->connectToHost(host, port);
+}
+
+void Forwarder::setupTunnel(const QByteArray &, const QByteArray &rawUrl, const QByteArray &protocol)
+{
+    disconnect(m_client, &QTcpSocket::readyRead, this, &Forwarder::handleRequest);
+
+    m_tunnel = new QTcpSocket(this);
+    connect(m_tunnel, &QTcpSocket::readyRead, this, &Forwarder::forwardResponse);
+    connect(m_tunnel, (void (QAbstractSocket::*)(QAbstractSocket::SocketError)) &QTcpSocket::error, [=](QAbstractSocket::SocketError){
+        m_client->disconnectFromHost();
+    });
+
+    QList<QByteArray> parts = rawUrl.split(':');
+    QString host = QString::fromLatin1(parts.at(0));
+    ushort port = 80;
+    if (parts.length() > 1)
+        port = parts.at(1).toUShort();
+
+    QByteArray body = readAll();
+
+    if (isBlocked(host)) {
+        QByteArray url = rawUrl;
+        filterRequest(url, true);
+        filterRequest(body, true);
+
+        connect(m_tunnel, &QTcpSocket::connected, [=](){
+            m_tunnel->write("CHYOUSA true *\r\n");
+            m_tunnel->write("CONNECT ");
+            m_tunnel->write(url);
+            m_tunnel->write(" ");
+            m_tunnel->write(protocol);
+            m_tunnel->write("\r\n");
+            m_tunnel->write(body);
+
+            connect(m_client, &QTcpSocket::readyRead, this, &Forwarder::forwardRequest);
+        });
+        m_tunnel->connectToHost("p.takashiro.me", 5526);
+    } else {
+        connect(m_tunnel, &QTcpSocket::connected, [=](){
+            connect(m_client, &QTcpSocket::readyRead, this, &Forwarder::forwardRequest);
+            m_client->write("HTTP/1.1 200 Connection Established\r\n\r\n");
+        });
+        m_tunnel->connectToHost(host, port);
+    }
+}
+
+void Forwarder::setFilter(const QByteArray &, const QByteArray &status, const QByteArray &)
+{
+    m_enableFilter = (status == "true");
 }
--- forwarder.h
+++ forwarder.h
@@ -9,6 +9,8 @@ class QTcpSocket;
 
 class Forwarder : public QObject
 {
+    friend struct ForwarderInit;
+
 public:
     Forwarder(QTcpSocket *client, QObject *parent = nullptr);
 
@@ -17,7 +19,9 @@ public:
 
 protected:
     void handleRequest();
-    QByteArray readBody();
+
+    QByteArray readLine();
+    QByteArray readAll();
 
     static bool isBlocked(const QString &domain);
     static void filterRequest(QByteArray &data, bool forward);
@@ -28,6 +32,13 @@ protected:
     QTcpSocket *m_client;
     QTcpSocket *m_proxy;
     QTcpSocket *m_tunnel;
+    bool m_enableFilter;
+
+    typedef void (Forwarder::*CommandHandler)(const QByteArray &, const QByteArray &, const QByteArray &);
+    void directRequest(const QByteArray &method, const QByteArray &rawUrl, const QByteArray &protocol);
+    void setupTunnel(const QByteArray &, const QByteArray &rawUrl, const QByteArray &protocol);
+    void setFilter(const QByteArray &, const QByteArray &status, const QByteArray &);
+    static QMap<QByteArray, CommandHandler> m_handlers;
 };
 
 #endif // FORWARDER_H
